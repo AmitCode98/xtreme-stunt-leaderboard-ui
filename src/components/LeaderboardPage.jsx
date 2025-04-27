c88c4c8b8f034d4d24b36fe24cf43f15679e7336
Your Name <you@example.com>
2025-04-27 19:23:34 +0530

    Initial LeaderboardPage build done — minor refinements pending

--- src/components/LeaderboardPage.jsx
+++ src/components/LeaderboardPage.jsx
@@ -26,12 +26,14 @@ const LeaderboardPage = () => {
       className="relative min-h-screen bg-center bg-cover"
       style={{ backgroundImage: `url(${BackGroundImg})` }}
     >
-      <div className="relative flex flex-col items-center justify-center min-h-screen p-4">
-        <img src={Logo} alt="Game Logo" className="w-52" />
-        <h2 className=" text-white font-exo  text-center text-2xl font-semibold mb-4 uppercase tracking-wide">
+      <div className="relative flex flex-col items-center justify-center min-h-screen p-4 overflow-hidden">
+        <div className="absolute top-1/2 left-1/2 w-[300px] h-[300px] sm:w-[500px] sm:h-[500px] md:w-[700px] md:h-[700px] lg:w-[800px] lg:h-[900px] bg-gradient-to-r from-sky-500 to-sky-600 opacity-70 blur-2xl sm:blur-3xl rounded-full transform -translate-x-1/2 -translate-y-1/3 z-0 border"></div>
+        {/* logo */}
+        <img src={Logo} alt="Game Logo" className="w-52 z-10" />
+        <h2 className=" text-white font-exo  text-center text-2xl font-semibold mb-4 uppercase tracking-wide z-10">
           Global Leaderboard
         </h2>
-        <div className="bg-white/30 border border-white/70 backdrop-blur-md rounded-2xl p-6 w-full max-w-lg shadow-xl overflow-hidden">
+        <div className="bg-white/30 border border-white/70 backdrop-blur rounded-2xl p-5 w-full max-w-lg shadow-2xl overflow-hidden">
           <div className="scrollbar overflow-y-auto max-h-[400px] pr-2 space-y-4">
             {players.map((player, index) => (
               <div
